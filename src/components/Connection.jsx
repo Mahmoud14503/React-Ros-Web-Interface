1ba2a8544509ddda4f01f5fd6ec4d81b14ec572a
Mahmoud14503 <mahmoudmohammedmm805@gmail.com>
2025-05-11 03:07:01 +0200

    Orientation, Connection

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
-import Alert from 'react-bootstrap/Alert';
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+import ROSLIB from 'roslib';
 
-class Connection extends Component {
-    state = { connected: false, ros: null } 
-    constructor() {
-        super();
-        setInterval(() => {
-            this.setState(prevState => ({connected: !prevState.connected}))
-        }, 1000)
+class ConnectionStatus extends React.Component {
+    // NOTE: the ros.on function is executed once 
+    // SO YOU NEED TO REGRESH EACH TIME YOU START THE SERVER 
+    state = { connected: false, ros: null };
+
+    componentDidMount() {
+        this.rosConnection();
     }
-    render() { 
+
+    rosConnection() {
+            this.state.ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' });
+            
+            this.state.ros.on('connection', () => {
+                this.setState({connected: true})
+                console.log('connected successfully to the websocket');
+            });
+    
+            this.state.ros.on('error', (error) => {
+                this.setState({connected: false})
+                console.log(`errored out: ${error}`);
+            });
+    
+            // When the Rosbridge server shuts down, fill the "status" span with "closed"
+            this.state.ros.on('close', () => {
+                this.setState({connected: false})
+                console.log('connection closed');
+            });
+        }
+
+    render() {
         return (
-            <Alert className='text-center m-3' variant={this.state.connected?"success":"danger"}>{this.state.connected?"Connected":"Disconnected"}</Alert>
+            <Alert className='text-center m-3' variant={this.state.connected ? "success" : "danger"}>
+                {this.state.connected ? "Connected" : "Disconnected"}
+            </Alert>
         );
     }
 }
 
-export default Connection;
+export default ConnectionStatus;
--- src/components/RobotState.jsx
+++ src/components/RobotState.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import {Row, Col, Container} from 'react-bootstrap'
 import * as ROSLIB from 'roslib';
+import * as Three from "three";
 
 class RobotState extends Component {
     state = { ros: null, x: 0, y:0, orientation:0, linear_velocity:0, angular_velocity:0, linear_acceleration:0 } 
 
-    constructor() {
-        super();
-    }
     componentDidMount() {
         this.rosConnection();
         this.getRobotStates();
     }
     rosConnection() {
         this.state.ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' });
-
+        
         this.state.ros.on('connection', () => {
             // setConnectionStatus('successful');
             // setConnection(true);
@@ -54,10 +52,23 @@ class RobotState extends Component {
             const x = message.pose.pose.position.x;
             const y = message.pose.pose.position.y;
             const angular_velocity = message.twist.twist.angular.z;
-            const linear_velocity = Math.sqrt(message.twist.twist.linear.x**2 + message.twist.twist.linear.y**2);
-            this.setState({ x, y, angular_velocity, linear_velocity });
+            const linear_velocity = message.twist.twist.linear.x;
+            const orientation = this.computeOrientation(message.pose.pose.orientation);
+            this.setState({ x, y, orientation, angular_velocity, linear_velocity });
         });
         
+    }
+    computeOrientation(ros_orientation_quaternion) {
+        var q = new Three.Quaternion(
+            ros_orientation_quaternion.x,
+            ros_orientation_quaternion.y,
+            ros_orientation_quaternion.z,
+            ros_orientation_quaternion.w
+          );
+          //convert this quaternion into Roll, Pitch and Yaw
+          var RPY = new Three.Euler().setFromQuaternion(q);
+      
+          return RPY["_z"];
     }
     render() { 
         return (
@@ -67,11 +78,12 @@ class RobotState extends Component {
                         <h4 className='mt-4'>Position:</h4>
                         <p className='mt-0'>x: {this.state.x} </p>
                         <p className='mt-0'>y: {this.state.y} </p>
-                        <p className='mt-0'>orientation: </p>
+                        <p className='mt-0'>orientation in radian: {this.state.orientation} </p>
+                        <p className='mt-0'>orientation in degrees: {this.state.orientation * (180 / Math.PI)} </p>
                     </Col>
                     <Col>
                         <h4 className='mt-4'>Velocities:</h4>
-                        <p className='mt-0'>linear velocity: *x^ + y^* = {this.state.linear_velocity} </p>
+                        <p className='mt-0'>linear velocity: {this.state.linear_velocity} </p>
                         <p className='mt-0'>angular velocity: {this.state.angular_velocity} </p>
                         <p className='mt-0'>linear acceleration: {this.state.linear_acceleration} </p>
                     </Col>
